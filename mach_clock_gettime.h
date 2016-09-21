23abac4a41540d10f2d6b98f885cdf57e7915182
albert <albert.rafetseder+github@univie.ac.at>
2016-09-21 16:57:04 +0200

    Finalize `mach_clock_gettime` library
    * Add proper docstring documenting what the library does, what it
      is based on, etc.
    * Make sure that types, the function signature, and implementation
      details are referenced.
    * Adapt style (braces, block comment) to https://github.com/br101/horst's.

--- mach_clock_gettime.h
+++ mach_clock_gettime.h
@@ -1,22 +1,20 @@
 /*
- * mach_clock_gettime --- an emulation of POSIX's `clock_gettime` for 
+ * mach_clock_gettime --- an emulation of POSIX's `clock_gettime` for
  * Mach / Mac OS X.
  *
- * This little library is based on material by
- * - The Open Group, see 
- *     http://pubs.opengroup.org/onlinepubs/9699919799/functions/clock_gettime.html
- * - the Linux kernel headers, see posix_types.h and types.h
- * - https://stackoverflow.com/questions/11680461/monotonic-clock-on-osx
+ * See mach_clock_gettime.c for details.
  */
 
-#ifndef _MACH_CLOCK_GETTIME_
-#define _MACH_CLOCK_GETTIME_
+#ifndef _MACH_CLOCK_GETTIME_H_
+#define _MACH_CLOCK_GETTIME_H_
 
-#include<time.h>
+#include <time.h>
 
-#define CLOCK_MONOTONIC 1
-typedef int clockid_t;
+#define CLOCK_MONOTONIC 1	/* Per Linux's time.h */
+typedef int clockid_t;		/* Per Linux's types.h, posix_types.h */
 
+/* Per the POSIX Realtime Extensions */
 int clock_gettime(clockid_t clock_id, struct timespec *tp);
 
 #endif
+
